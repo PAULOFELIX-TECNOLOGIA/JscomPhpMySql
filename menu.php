6d5634ce65408fa7a9f3727ec4e4963124e41a00
PAULOFELIX-TECNOLOGIA <73800800+PAULOFELIX-TECNOLOGIA@users.noreply.github.com>
2024-11-19 08:45:30 -0300

    5 commit

--- menu.php
+++ menu.php
@@ -44,29 +44,6 @@
             font-size: 24px;
         }
 
-        /* Modal */
-        .modal {
-            display: none;
-            position: fixed;
-            z-index: 1;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            overflow: auto;
-            background-color: #007bff;
-            background-color: rgba(0, 0, 0, 0.4);
-            padding-top: 60px;
-        }
-
-        .modal-content {
-            background-color: #007bff;
-            margin: 5% auto;
-            padding: 20px;
-            border: 1px solid #888;
-            width: 80%;
-        }
-
         .close {
             color: #aaa;
             float: right;
